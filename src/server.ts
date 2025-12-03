f4880b2385301e1b0de65946b014c3ce16dd843f
K M Marziul Karim <marziulrafi@gmail.com>
2025-12-03 12:38:11 +0600

    server replaced by user.routes

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
-import express, { NextFunction, Request, Response } from "express"
+import express, { Request, Response } from "express"
 import config from "./config";
 import initDB, { pool } from "./config/db";
+import { logger } from "./middleware/logger";
+import { userRoutes } from "./modules/user/user.routes";
 
 
 const app = express()
@@ -17,16 +19,6 @@ initDB()
 
 
 
-// Logger Middleware
-
-const logger = (req: Request, res: Response, next: NextFunction) => {
-  console.log(`$[{new Date().toISOString()}] ${req.method} ${req.path}\n`)
-
-  next()
-}
-
-
-
 app.get('/', logger, (req: Request, res: Response) => {
   res.send('Hello!')
 })
@@ -35,46 +27,8 @@ app.get('/', logger, (req: Request, res: Response) => {
 
 // Users CRUD
 
-app.post('/users', async (req: Request, res: Response) => {
-  const { name, email } = req.body
+app.use('/users', userRoutes)
 
-  try {
-    const result = await pool.query(`INSERT INTO users(name, email) VALUES($1, $2) RETURNING *`, [name, email])
-
-    // console.log(result.rows[0]);
-    res.status(201).json({
-      success: true,
-      message: "Data Inserted",
-      data: result.rows[0],
-    })
-
-  } catch (err: any) {
-    res.status(500).json({
-      success: false,
-      message: err.message
-    })
-  }
-})
-
-
-
-app.get("/users", async (req: Request, res: Response) => {
-  try {
-    const result = await pool.query(`SELECT * FROM users`)
-
-    res.status(200).json({
-      success: true,
-      message: "Users retrieved successfully",
-      data: result.rows
-    })
-
-  } catch (err: any) {
-    res.status(500).json({
-      success: false,
-      message: err.message
-    })
-  }
-})
 
 
 app.get('/users/:id', async (req: Request, res: Response) => {
