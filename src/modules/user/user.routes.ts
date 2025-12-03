c468162285d4f4730325b0b32aa870c3ccaa23d1
K M Marziul Karim <marziulrafi@gmail.com>
2025-12-03 20:47:13 +0600

    Modular Pattern for users done

--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -1,48 +1,14 @@
 import { Router } from "express";
-import { Request, Response } from "express"
-import { pool } from "../../config/db";
+import { userControllers } from "./user.controller";
 
 const router = Router()
 
-router.post("/", async (req: Request, res: Response) => {
-  const { name, email } = req.body
+router.post("/", userControllers.createUser)
+router.get("/", userControllers.getUser)
+router.get("/:id", userControllers.getSingleUser)
+router.put("/:id", userControllers.updateUser)
+router.delete("/:id", userControllers.deleteUser)
 
-  try {
-    const result = await pool.query(`INSERT INTO users(name, email) VALUES($1, $2) RETURNING *`, [name, email])
-
-    // console.log(result.rows[0]);
-    res.status(201).json({
-      success: true,
-      message: "Data Inserted",
-      data: result.rows[0],
-    })
-
-  } catch (err: any) {
-    res.status(500).json({
-      success: false,
-      message: err.message
-    })
-  }
-})
-
-
-router.get("/", async (req: Request, res: Response) => {
-  try {
-    const result = await pool.query(`SELECT * FROM users`)
-
-    res.status(200).json({
-      success: true,
-      message: "Users retrieved successfully",
-      data: result.rows
-    })
-
-  } catch (err: any) {
-    res.status(500).json({
-      success: false,
-      message: err.message
-    })
-  }
-})
 
 
 
